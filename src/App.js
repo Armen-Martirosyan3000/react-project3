f3dd3e1400e8e682f1d301533445215e64135094
Hasul79 <hatuk2@mail.ru>
2022-08-19 23:36:12 +0400

    finish login panel

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router,  Route, Routes  } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Home from './components/home.component';
-import Nav from './components/nav.component';
-import Login from './components/login.component';
-import Register from './components/register.component';
+import React from "react";
+import "./style.css";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-    <div className="App">
-         <Nav/>
-             <div className="auth-wrapper">
-                <div className="auth-inner">
-                  <Routes>
-                     <Route exact path="/" component={Home} />
-                     <Route exact path="/login" component={Login} />
-                     <Route exact path="/register" component={Register} />
-                  </Routes>
-         <Home/>
-            </div>
-         </div>
+    <div>
+      <h1></h1>
+      <p></p>
     </div>
-    </Router>
   );
 }
 
-export default App;
+
 
